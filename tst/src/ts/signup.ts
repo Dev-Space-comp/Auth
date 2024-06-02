a02066eb3ad4f95ac44dd35dbcca4079bd002013
RUSHIL GUPTA <rushiling121@gmail.com>
2024-06-02 14:41:33 +0530

    1

--- tst/src/ts/signup.ts
+++ tst/src/ts/signup.ts
@@ -1,11 +1,11 @@
-const form = document.querySelector("form")
+const form = document.querySelector("form")  as HTMLFormElement;
 
 
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting the traditional way
 
     const form_Data = new FormData(this);
-    const formData = {};
+    const formData:{ [key: string]: FormDataEntryValue } = {};
     form_Data.forEach((value, key) => {
         // console.log(`${key}: ${value}`);
         formData[key] = value
